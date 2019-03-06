2e24058ec917b468e3ae559a7fbf7e982af5fcef
Samiran Das <samiran@live.com>
2019-03-06 00:50:44 -0500

    Create DHT11_RPi.py
    This code is used to get data i.e. Temperature and Humidity from DHT11 and prints the same simultaneously using RPi.

---
+++ DHT11_RPi.py
@@ -0,0 +1,16 @@
+# Get Humidiity & Temperature from DHT11
+# Prints Temperature & Humidity rapidly after intervals of 1 sec
+# Samiran Das - github.com/samiran-das
+
+import sys
+import Adafruit_DHT
+import time
+
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    time.sleep(1)
+    
+    # End of Code
