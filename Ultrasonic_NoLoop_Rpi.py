122f79b05548d81ca48982b4e863dadeb51c5063
Samiran Das <samiran@live.com>
2019-03-06 01:14:39 -0500

    Create Ultrasonic_NoLoop_Rpi.py
    This code is use to measure the distance from an object to the ultrasonic sensor and prints the same in cms using Raspberry Pi.

---
+++ Ultrasonic_NoLoop_Rpi.py
@@ -0,0 +1,43 @@
+
+  
+# Initiates the Ultrasonic Sensor and get data by measuring the distance from an object to the sensor
+# Prints the data i.e. the distance in cms on the screen
+# Samiran Das - github.com/samiran-das
+
+import RPi.GPIO as GPIO
+
+import time
+
+GPIO.setmode(GPIO.BCM)
+TRIG = 23
+
+ECHO = 24
+print "Distance Measurement In Progress"
+GPIO.setup(TRIG,GPIO.OUT)
+
+GPIO.setup(ECHO,GPIO.IN)
+GPIO.output(TRIG, False)
+
+print "Waiting For Sensor To Settle"
+
+time.sleep(2)
+
+GPIO.output(TRIG, True)
+
+time.sleep(0.00001)
+
+GPIO.output(TRIG, False)
+while GPIO.input(ECHO)==0:
+
+  pulse_start = time.time()
+while GPIO.input(ECHO)==1:
+
+  pulse_end = time.time()
+pulse_duration = pulse_end - pulse_start
+distance = pulse_duration * 17150
+distance = round(distance, 2)
+print "Distance:",distance,"cm"
+GPIO.cleanup()
+
+# End of Code
+
